be1459e3b0fa75f8f76973a9e9f1286bca0c2797
Marian Burlacu <marian.burlacu@edu.unito.it>
2023-08-24 11:38:43 +0200

    Header section in the Feed Component

---
+++ components/Feed.jsx
@@ -0,0 +1,18 @@
+import { SparklesIcon } from "@heroicons/react/24/outline";
+
+const Feed = () => {
+    return (
+        <div className="sm:ml-[73px] xl:ml-[370px] border-l border-r border-gray-200 xl:min-w-[576px] flex-grow max-w-xl">
+            <div className="flex justify-between py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200">
+                <h2 className="text-lg am:text-xl font-bold cursor-pointer">
+                    Home
+                </h2>
+                <div className="hoverEffect flex items-center justify-center px-0 w-9 h-9">
+                    <SparklesIcon className="h-5 " />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Feed;
